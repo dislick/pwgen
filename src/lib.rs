0ad252824400b97d6baf5fcce1c9e095573c6e56
Patrick Muff <muff.pa@gmail.com>
2020-05-15 22:49:35 +0200

    feat: add copy to clipboard feature

--- src/lib.rs
+++ src/lib.rs
@@ -4,23 +4,47 @@ mod options;
 
 #[macro_use]
 extern crate clap;
+extern crate clipboard;
 
 pub use crate::charset::*;
 pub use crate::consts::*;
 pub use crate::options::*;
+use clipboard::ClipboardContext;
+use clipboard::ClipboardProvider;
 use rand::Rng;
 
-pub fn run(
-  options: &Options,
-  options_secret: &GeneratorOptions,
-) -> Result<(), Box<dyn std::error::Error>> {
+pub fn run(options: &Options) -> Result<(), Box<dyn std::error::Error>> {
   match options.subcommand {
+    // Mode: Default
     SubCommand::None => {
-      for _ in 0..options.generator_options.count {
-        println!("{}", gen_password(&options.generator_options));
+      if options.generator_options.count == 1 {
+        let pass = gen_password(&options.generator_options);
+        let mut ctx: ClipboardContext = ClipboardProvider::new()?;
+        println!("{}", pass);
+        ctx.set_contents(pass)?;
+      } else {
+        for _ in 0..options.generator_options.count {
+          println!("{}", gen_password(&options.generator_options));
+        }
       }
     }
-    SubCommand::Secret => println!("{}", gen_password(&options_secret)),
+    // Mode: Secret
+    SubCommand::Secret => {
+      let secret_charsets = vec![
+        Charset::new(&CHARSET_ALPHABET),
+        Charset::new(&CHARSET_ALPHABET_UPPERCASE),
+        Charset::new(&CHARSET_NUMBERS),
+      ];
+      let options_secret = GeneratorOptions {
+        length: get_length_for_entropy(
+          MINIMUM_ENTROPY_IN_BITS,
+          count_chars_in_charsets(&secret_charsets),
+        ),
+        count: 1,
+        charsets: secret_charsets,
+      };
+      println!("{}", gen_password(&options_secret))
+    }
   }
 
   Ok(())
@@ -30,14 +54,14 @@ pub fn gen_password(options: &GeneratorOptions) -> String {
   let mut password = String::new();
 
   for _ in 0..options.length {
-    let charset = get_random_charset(&options.charsets);
+    let charset = get_rand_charset(&options.charsets);
     password.push(charset.get_rand_char());
   }
 
   password
 }
 
-fn get_random_charset(charsets: &Vec<Charset>) -> &Charset {
+fn get_rand_charset(charsets: &Vec<Charset>) -> &Charset {
   let rand_index = rand::thread_rng().gen_range(0, charsets.len());
   &charsets[rand_index]
 }
--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,7 @@ fn main() {
     // Parse cli arguments
     let options = pwgen::Options::from_args();
 
-    // Static options for subcommands
-    let secret_charsets = vec![
-        Charset::new(&pwgen::CHARSET_ALPHABET),
-        Charset::new(&pwgen::CHARSET_ALPHABET_UPPERCASE),
-        Charset::new(&pwgen::CHARSET_NUMBERS),
-    ];
-    let options_secret = GeneratorOptions {
-        length: get_length_for_entropy(
-            MINIMUM_ENTROPY_IN_BITS,
-            count_chars_in_charsets(&secret_charsets),
-        ),
-        count: 1,
-        charsets: secret_charsets,
-    };
-
-    if let Err(e) = run(&options, &options_secret) {
+    if let Err(e) = run(&options) {
         eprintln!("Application error: {}", e);
         process::exit(1);
     }
